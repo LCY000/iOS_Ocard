3bf9c9e46b934aef678df522099267296832ea2c
CYouuu <zqwe1230@gmail.com>
2026-03-27 16:48:17 +0800

    Initial Commit

---
+++ Hw2_AppUI/Hw2_AppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hw2_AppUIApp.swift
+//  Hw2_AppUI
+//
+//  Created by ChengYou on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct Hw2_AppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
